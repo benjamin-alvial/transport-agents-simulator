78d41996706e481ea7bce0feecc3b016cadc9205
benjamin-alvial <117480317+benjamin-alvial@users.noreply.github.com>
2026-01-20 11:06:23 +0100

    clean up examples

--- examples/delivering.py
+++ examples/delivering.py
@@ -4,22 +4,28 @@ if __name__ == "__main__":
     sim = Kernel()
 
     # Create agents
-    uber = Platform("uber")
+    platform = Platform("platform")
     customer_sending = Customer("customer_sending", 10)
-    courier = Courier("courier", 0, 20, 30)
+    courier1 = Courier("courier1", 0, 20, 30)
+    courier2 = Courier("courier2", 0, 20, 30)
 
-    sim.register_agent(uber)
+    sim.register_agent(platform)
     sim.register_agent(customer_sending)
-    sim.register_agent(courier)
+    sim.register_agent(courier1)
+    sim.register_agent(courier2)
 
     sim.schedule(delay=5.0,
                  action=customer_sending.send_delivery_request,
                  data={"parcel": Parcel("laptop", customer_sending.location, 100, 5, 10),
-                       "platform": uber})
+                       "platform": platform})
 
     sim.schedule(delay=5.0,
-                 action=uber.register_courier,
-                 data={"courier": courier})
+                 action=platform.register_courier,
+                 data={"courier": courier1})
+
+    sim.schedule(delay=5.0,
+                 action=platform.register_courier,
+                 data={"courier": courier2})
 
     print("Starting simulation...\n")
     sim.run(until=100.0)
