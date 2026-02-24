d849b17f494b79e7affa229a08d2cf2fcf307d09
benjamin-alvial <117480317+benjamin-alvial@users.noreply.github.com>
2026-02-24 17:15:00 +0100

    Cleanup for major refactor

--- parcel_delivery/models/occupancy.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Occupancy:
-    """Represents a vehicle occupying an edge during a time interval"""
-    vehicle_id: str
-    vehicle_type: str  # "bus", "courier", "car"
-    enter_time: float
